74f43a6804e3e1959701d3df03cc9cf6bbe276a9
kabagemark <kabagemark@gmail.com>
2018-09-28 11:48:19 +0300

    static setting set for uploading images

--- gallery/models.py
+++ gallery/models.py
@@ -2,18 +2,18 @@ from django.db import models
 
 # Create your models here.
 
-class Image(models.Model):
-    name = models.CharField(max_length=60)
-    image = models.ImageField(upload_to = 'articles/')
-    descripition = models.TextField()
-    location = models.ForeignKey(Editor)
-    category = models.ManyToManyField(tags)
-    pub_date = models.DateTimeField(auto_now_add=True)
-
-
 class Location(models.Model):
     name = models.CharField(max_length=60)
 
 
 class Category(models.Model):
     name = models.CharField(max_length=60) 
+
+class Image(models.Model):
+    name = models.CharField(max_length=60)
+    image = models.ImageField(upload_to = 'articles/')
+    descripition = models.TextField()
+    location = models.ForeignKey(Location)
+    category = models.ManyToManyField(Category)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
--- gallery/urls.py
+++ gallery/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url,include
 from django.conf import settings
+from django.conf.urls.static import static
 from .import views
 
 urlpatterns=[
     url(r'^',views.Gallery,name = 'gallery'),
-    ] 
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)     
