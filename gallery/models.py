7334f2d89488fca4069c93c18913ab9a7afe2e83
kabagemark <kabagemark@gmail.com>
2018-09-28 11:24:08 +0300

    Image model added

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Image(models.Model):
+    name = models.CharField(max_length=60)
+    image = models.ImageField(upload_to = 'articles/')
+    descripition = models.TextField()
+    location = models.ForeignKey(Editor)
+    category = models.ManyToManyField(tags)
+    pub_date = models.DateTimeField(auto_now_add=True)
+   
+ 
