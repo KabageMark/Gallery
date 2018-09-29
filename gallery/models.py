5f7daf9a88fd0565bf68c85e19e3b680335942a8
kabagemark <kabagemark@gmail.com>
2018-09-29 11:09:31 +0300

    filter images by categorys added

--- gallery/models.py
+++ gallery/models.py
@@ -5,21 +5,27 @@ from django.db import models
 class Location(models.Model):
     name = models.CharField(max_length=60)
 
+    def __str__(self):
+        return self.name
 
 class Category(models.Model):
-    name = models.CharField(max_length=60) 
+    name = models.CharField(max_length=60,blank=True)
+
+    def __str__(self):
+        return self.name
+ 
 
 class Image(models.Model):
     name = models.CharField(max_length=60)
     image = models.ImageField(upload_to = 'gallery/')
     descripition = models.TextField()
     location = models.ForeignKey(Location)
-    category = models.ManyToManyField(Category)
+    category = models.ForeignKey(Category)
     pub_date = models.DateTimeField(auto_now_add=True)
     
     @classmethod
-    def display_image(cls):
-        images = cls.objects.all()
+    def display_image(cls,category):
+        images = cls.objects.filter(category__name=category)
         return images
 
     def search_by_category(cls,search_term):
--- gallery/views.py
+++ gallery/views.py
@@ -2,15 +2,18 @@ from django.shortcuts import render,redirect
 from .models import Image,Location,Category
 # Create your views here.
 def Gallery(request):
-    image = Image.display_image()
-    return render(request, 'index.html',{"image":image})
+    personal = Image.display_image('personal')
+    travel = Image.display_image('travel')
+    sports = Image.display_image('sports')
+    family= Image.display_image('family')
+    return render(request, 'index.html',{"personal":personal,"travel":travel,"sports":sports,"family":family})
 
 def search_results(request):
 
     if 'image' in request.GET and request.GET["image"]:
         search_term = request.GET.get("image")
         searched_images = Image.search_by_category(search_term)
-        result_images = f"{search_term}"
+        message = f"{search_term}"
 
         return render(request, 'searched.html',{"message":message,"searched_images": searched_images})
 
